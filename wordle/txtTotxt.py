1480e4fe416f8e1de41c3cd52c8751584dd75866
gigi parmentier <tristan.boin@gmail.com>
2022-01-08 19:00:23 +0100

    fixes wordle bugs

--- wordle/txtTotxt.py
+++ wordle/txtTotxt.py
@@ -9,6 +9,12 @@ with open('existing_words.txt') as file:
         if len(word) == 5:
             words.append(word.lower())
 
+with open('dict_small.txt') as file:
+    for row in file:
+        word = row.rstrip()
+        if word not in words:
+            words.append(word.lower())
+
 print(len(words))
 
 with open('dict_big.txt','w') as f:
